0fffb3c17034b4b1b53c8a53881c6e919b0b29c0
SonHua <hualocson@gmail.com>
2026-07-23 19:56:59 +0700

    refactor(server): add timestamps, indexes, toJSON transform, InferSchemaType to Post model

--- server/mongodb/models/post.ts
+++ server/mongodb/models/post.ts
@@ -1,16 +1,25 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema, InferSchemaType } from 'mongoose'
 
-export interface IPost {
-    name: string
-    prompt: string
-    photo: string
-}
-
-const PostSchema = new Schema<IPost>({
-    name: { type: String, required: true },
+const PostSchema = new Schema({
+    name: { type: String, required: true, trim: true, index: true },
     prompt: { type: String, required: true },
     photo: { type: String, required: true },
+}, {
+    timestamps: true,
+    strict: 'throw',
+    toJSON: {
+        virtuals: true,
+        transform(_doc: any, ret: any) {
+            ret.id = ret._id.toString()
+            delete ret._id
+            delete ret.__v
+            return ret
+        },
+    },
 })
 
-const Post = mongoose.model('Post', PostSchema)
-export default Post
+PostSchema.index({ createdAt: -1 })
+
+export type PostDocument = InferSchemaType<typeof PostSchema>
+export const PostModel = mongoose.model('Post', PostSchema)
+export default PostModel
