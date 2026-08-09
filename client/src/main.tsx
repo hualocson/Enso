75f0c1fd855febc09d2d7a55a02298ff6de90425
SonHua <hualocson@gmail.com>
2026-08-09 11:12:34 +0700

    UPDATE Tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,23 +6,25 @@ import App from './App'
 import './index.css'
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 60_000,
-            retry: 1,
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000,
+      retry: 1,
+      gcTime: 60 * 60 * 1000,
+      refetchOnWindowFocus: false,
     },
+  },
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <Toaster
-                position="bottom-right"
-                richColors
-                closeButton
-            />
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="bottom-right"
+        richColors
+        closeButton
+      />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
 )
